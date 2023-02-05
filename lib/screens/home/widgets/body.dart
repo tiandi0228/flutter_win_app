582997654b661f09290933f0cd229f0aa4c277bb
Hongcha <tiandi0228@live.com>
2023-02-05 21:09:41 +0800

    update: 调整代码

--- lib/screens/home/widgets/body.dart
+++ lib/screens/home/widgets/body.dart
@@ -16,7 +16,33 @@ class BodyState extends State<Body> {
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
       color: const Color.fromRGBO(253, 246, 227, 1),
-      child: const Text('1111111'),
+      child: Column(
+        children: [
+          GridView.count(
+            crossAxisCount: 3,
+            shrinkWrap: true,
+            mainAxisSpacing: 12,
+            crossAxisSpacing: 12,
+            childAspectRatio: 3,
+            children: [
+              _buildBox('1'),
+              _buildBox('2'),
+              _buildBox('3'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBox(String text) {
+    return Container(
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(238, 232, 213, 1),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Text(text),
     );
   }
 }
